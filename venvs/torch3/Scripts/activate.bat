65b020ad5ccfd36c1d773bfb9611c845d85c11ba
medine.balci <medine.balci@uzh.ch>
2025-04-20 15:31:35 +0200

    reinstall

--- venvs/torch3/Scripts/activate.bat
+++ venvs/torch3/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\medin\OneDrive\Dokumente\mt-exercise-03\mt-exercise-03\venvs\torch3
+set VIRTUAL_ENV=C:\Users\medin\OneDrive\Dokumente\mt-exercise-03\venvs\torch3
 
 if not defined PROMPT set PROMPT=$P$G
 
